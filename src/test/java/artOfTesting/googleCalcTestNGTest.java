2206dc609ec767dce2eea44538dcc412163cc494
Maksim Sarychau <maksim_sarychau@epam.com>
2015-05-28 14:31:55 +0200

    Using testng 1.2.2

--- src/test/java/artOfTesting/googleCalcTestNGTest.java
+++ src/test/java/artOfTesting/googleCalcTestNGTest.java
@@ -2,14 +2,14 @@ package artOfTesting;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-import cucumber.api.testng.CucumberFeatureWrapper;
 
 
 @CucumberOptions(
-	       format={"pretty", "json:target/report.json"},
-	       //features = "classpath:basic_arithmetic.feature"
-	       //,   
-	       tags="@important,@important2"
+			format={"pretty", "html:target/cucumber"}
+		   //format={"pretty", "json:target/report.json"}
+	       //,
+	       //,features = "classpath:artOfTesting/test/googleCalcSecondTest.feature"   
+	       //,tags="@important"
 
 	       )
 public class googleCalcTestNGTest extends AbstractTestNGCucumberTests {
